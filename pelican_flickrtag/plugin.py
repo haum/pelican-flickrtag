fa210fa6f17187808a8427e07b3f43697b7fcfc6
JbLb <jerome@jblb.net>
2015-02-16 18:57:18 +0100

    add code to insert images in pages

--- pelican_flickrtag/plugin.py
+++ pelican_flickrtag/plugin.py
@@ -149,7 +149,87 @@ def replace_article_tags(generator):
 
 
 def replace_page_tags(generator):
-    pass
+    from jinja2 import Template
+
+    api = generator.flickr_api_client
+    if api is None:
+        logger.error('[flickrtag]: Unable to get the Flickr API object')
+        return
+
+    tmp_file = generator.context.get('FLICKR_TAG_CACHE_LOCATION')
+
+    include_dimensions = generator.context.get('FLICKR_TAG_INCLUDE_DIMENSIONS')
+    size_alias = generator.context.get('FLICKR_TAG_IMAGE_SIZE')
+
+    photo_ids = set([])
+    logger.info('[flickrtag]: Parsing pages for photo ids...')
+    for page in generator.pages:
+        for match in flickr_regex.findall(page._content):
+            photo_ids.add(match[1])
+
+    logger.info('[flickrtag]: Found %d photo ids in the page' % len(photo_ids))
+
+    try:
+        with open(tmp_file, 'r') as f:
+            photo_mapping = pickle.load(f)
+    except (IOError, EOFError):
+        photo_mapping = {}
+    else:
+        # Get the difference of photo_ids and what have cached
+        cached_ids = set(photo_mapping.keys())
+        photo_ids = list(set(photo_ids) - cached_ids)
+
+    if photo_ids:
+        logger.info('[flickrtag]: Fetching photo information from Flickr...')
+        for id in photo_ids:
+            logger.info('[flickrtag]: Fetching photo information for %s' % id)
+            photo = api.Photo(id=id)
+            # Trigger the API call...
+            photo_mapping[id] = {
+                'title': photo.title,
+                'raw_url': url_for_alias(photo, size_alias),
+                'url': photo.url,
+            }
+
+            if include_dimensions:
+                sizes = photo.getSizes()
+                size = size_for_alias(sizes, size_alias)
+                photo_mapping[id]['width'] = size['width']
+                photo_mapping[id]['height'] = size['height']
+
+        with open(tmp_file, 'w') as f:
+            pickle.dump(photo_mapping, f)
+    else:
+        logger.info('[flickrtag]: Found pickled photo mapping')
+
+    # See if a custom template was provided
+    template_name = generator.context.get('FLICKR_TAG_TEMPLATE_NAME')
+    if template_name is not None:
+        # There's a custom template
+        try:
+            template = generator.get_template(template_name)
+        except Exception:
+            logger.error('[flickrtag]: Unable to find the custom template %s' % template_name)
+            template = Template(default_template)
+    else:
+        template = Template(default_template)
+
+    logger.info('[flickrtag]: Inserting photo information into pages...')
+    for page in generator.pages:
+        for match in flickr_regex.findall(page._content):
+            fid = match[1]
+            if fid not in photo_mapping:
+                logger.error('[flickrtag]: Could not find info for a photo!')
+                continue
+
+            # Create a context to render with
+            context = generator.context.copy()
+            context.update(photo_mapping[fid])
+
+            # Render the template
+            replacement = template.render(context)
+
+            page._content = page._content.replace(match[0], replacement)
 
 
 def register():
